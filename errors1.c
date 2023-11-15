3205061f5c36f647427a0e67ebe0aefcf8477a7b
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-15 15:23:40 +0000

    error_handling

---
+++ errors1.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+
+/**
+ * usage_error - Prints Usage error message.
+ * Return: EXIT_FAILURE.
+ */
+int usage_error(void)
+{
+	fprintf(stderr, "USAGE: monty file\n");
+	return (EXIT_FAILURE);
+}
+
+/**
+ * access_error - Prints Acces error message.
+ * Return: EXIT_FAILURE.
+ */
+int access_error(char *file)
+{
+	fprintf(stderr, "Error: Can't access file %s\n", file);
+	return (EXIT_FAILURE);
+}
+
+
+/**
+ * f_open_error - Prints f_open_error message.
+ * Return: EXIT_FAILURE.
+ */
+int f_open_error(char *file)
+{
+	fprintf(stderr, "Error: Can't open file %s\n", file);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * not_monty_file - Prints error message in case of wrong bytecode.
+ * Return: EXIT_FAILURE.
+ */
+int not_monty_file(char *file)
+{
+	fprintf(stderr, "Error: Not a Monty bytecode file %s\n", file);
+	exit(EXIT_FAILURE);
+}
---
+++ main.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * main - The main function.
+ * Return: An int.
+ */
+
+int main(int argc, char **argv)
+{
+	FILE *fd = NULL;
+	char *extension;
+	int exit_code = EXIT_SUCCESS;
+	
+	if (argc != 2)
+		return (usage_error());
+
+	extension = strrchr(argv[1], '.');
+	if (extension == NULL || strcmp(extension, ".m") != 0)
+		return (not_monty_file(argv[1]));
+
+	if (access(argv[1], F_OK) == -1)
+		return (access_error(argv[1]));
+
+	fd = fopen(argv[1], "r");
+	if (fd == NULL)
+		return (f_open_error(argv[1]));
+
+	/* call interpeter and return int to update exit_code */
+	/* exit_code = "pass fd to function" */
+
+	fclose(fd);
+	return (exit_code);
+}
