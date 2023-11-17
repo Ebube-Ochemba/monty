673b93809ca05e29643ae15f3d3d05ce50725c62
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-17 23:49:05 +0000

    specifier_update

--- opcodes1.c
+++ opcodes1.c
@@ -51,8 +51,32 @@ void _pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	fprintf(stdout, "%d\n", (*stack)->n);
+}
+
+/**
+ * _pop - Removes the top element of the stack.
+ * @stack: A pointer to the node at the top of the stack.
+ * @line_number: The line number in the monty file.
+ * Return: void
+ */
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+
+	free(temp);
 }
