404b657afe768bb469b1489de0c105a7914e89c2
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-18 13:43:05 +0000

    nop

--- opcodes2.c
+++ opcodes2.c
@@ -22,3 +22,15 @@ void _add(stack_t **stack, unsigned int line_number)
 	(*stack)->prev = NULL;
 	free(temp);
 }
+
+/**
+ * _nop - Doesn’t do anything.
+ * @stack: A pointer to the node at the top of the stack.
+ * @line_number: The line number in the monty file.
+ * Return: void
+ */
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
